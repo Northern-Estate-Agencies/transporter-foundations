039edaf7ae971233494b7b124d8d055f2a07ff87
Rory Lockett <rory.lockett@northernestateagencies.co.uk>
2023-02-28 12:30:21 +0000

    chore/adding a contact DTO

---
+++ src/Transporter/DataTransferObjects/ContactFromRequestData.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace TransporterFoundations\Transporter\DataTransferObjects;
+
+class ContactFromRequestData
+{
+    /**
+     * @param  array<mixed>  $metadata
+     */
+    public function __construct(
+        public string $id,
+        public string $title,
+        public string $forename,
+        public string $surname,
+        public string $email,
+        public bool $consentsToMarketing = false,
+    ) {
+    }
+
+    /**
+     * @param  array<string, array<string, string>|string>  $data
+     */
+    public static function fromPayload(array $data): self
+    {
+        return new self(
+            id: strval($data['ID']),
+            title: strval($data['title']),
+            forename: strval($data['forename']),
+            surname: strval($data['surname']),
+            email: strval($data['email']),
+            consentsToMarketing : ($data['marketingConsent'] === "given")
+        );
+    }
+}
